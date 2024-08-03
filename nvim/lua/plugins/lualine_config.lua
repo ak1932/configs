46ad38f1bc3e4b0685527d1f044d9160671b14ed
ak1932 <aryankadole95@gmail.com>
2024-08-03 08:50:26 +0530

    round everywhere!

--- nvim/lua/plugins/lualine_config.lua
+++ nvim/lua/plugins/lualine_config.lua
@@ -18,8 +18,8 @@ require('lualine').setup {
   options = {
     theme = "catppuccin",
     icons_enabled = true,
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
   },
