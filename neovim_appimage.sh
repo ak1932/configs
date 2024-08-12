550dd251fc47e929fb9526f24fde8687ab3eb31f
Aryan Kadole <72815172+ak1932@users.noreply.github.com>
2024-08-12 10:39:33 +0530

    Neovim appimage install script

---
+++ neovim_appimage.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+read -p "Download latest neovim appimage for linux? [y/n] " download
+if [[ $download == "y" ]]; then
+cd /tmp
+mkdir -p nvim
+cd nvim
+
+wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod +x nvim.appimage
+./nvim.appimage --appimage-extract
+mv squashfs-root $HOME/nvim
+read -p "Add downloaded appimage alias to bashrc? [y/n] " add_to_bash
+if [[ $add_to_bash == "y" ]]; then
+echo "alias nvim=$HOME/nvim/usr/bin/nvim" >> $HOME/.bashrc
+fi
+fi
+
+cd /tmp
+mkdir -p nvim
+cd nvim
+git clone https://github.com/ak1932/configs
+cd configs
+mv nvim ~/.config
